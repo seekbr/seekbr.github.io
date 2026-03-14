34efaa13a48a285fcf249e24b88338dba038794e
SeekBR <seekplayerbr@gmail.com>
2026-03-14 11:10:18 -0300

    Menu especial para mobile

--- script.js
+++ script.js
@@ -33,3 +33,11 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+const navbar = document.getElementById("navbar");
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  navbar.classList.toggle("active");
+});
